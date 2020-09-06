57aed6a6363c1d93f69f0899f7fe51eefa408805
LironBayda <34834883+LironBayda@users.noreply.github.com>
2020-09-06 16:36:39 +0300

    Add files via upload

---
+++ MyLibrary.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyLibary02092020
+{
+    class MyLibrary
+    {
+        private Dictionary<string, Book> _books;
+        public MyLibrary()
+        {
+            _books = new Dictionary<string, Book>();
+        }
+        public bool AddBook(Book book)
+        {
+           // if (_books.ContainsKey(book.Title))
+           //     return false;
+            _books.Add(book.Title, book);
+            return true;
+
+        }
+
+        public bool RemoveBook(string title)
+        {
+            if (_books.ContainsKey(title))
+            {
+               
+                _books.Remove(title);
+                return true;
+            }
+                return false;
+
+        }
+
+        public bool HaveThisBook(string title)
+        {
+            if (_books.ContainsKey(title))
+            {
+                return true;
+            }
+            return false;
+
+        }
+
+        public Book GetBook(string title)
+        {
+            if (_books.ContainsKey(title))
+            {
+                return _books[title];
+            }
+            return null;
+
+        }
+
+        public Book GetBookByAuthor(string author)
+        {
+            foreach (KeyValuePair<string, Book> book in _books)
+            {
+                if (book.Value.Author == author)
+                    return book.Value;
+            }
+
+            return null;
+        }
+
+        public void Clear()
+        {
+            _books.Clear();
+        }
+
+        public List<string> GetAuthors()
+        {
+            List<string> authors = new List<string>();
+            foreach (KeyValuePair<string, Book> book in _books)
+            {
+                authors.Add(book.Value.Author);
+            }
+
+            return authors;
+
+        }
+
+        
+            public List<Book> GetBooksSortedByAuthorName()
+        {
+            List<string> authors = GetAuthors();
+            authors.Sort();
+            List<Book> books = new List<Book>();
+            foreach (string author in authors)
+            {
+                books.Add(GetBookByAuthor(author));
+            }
+
+            return books;
+
+        }
+
+        public List<string> GetBooksTitleSorted()
+        {
+            List<string> books_title = new List<string>();
+            foreach (KeyValuePair<string, Book> book in _books)
+            {
+                books_title.Add(book.Value.Title);
+            }
+            books_title.Sort();
+            return books_title;
+
+        }
+
+        public int Count { 
+            get
+
+            {
+                return _books.Count;
+            } 
+        }
+
+        public override string ToString()
+        {
+            string str="";
+            foreach (KeyValuePair<string, Book> book in _books)
+            {
+                str += book.ToString();
+            }
+
+            return str;
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyLibary02092020
+{
+    class Program
+    {
+        static void display_books(List<Book> books)
+        {
+            foreach (Book book in books)
+            {
+                Console.WriteLine(book.ToString());
+            }
+        }
+        static void Main(string[] args)
+        {
+            MyLibrary myLibrary = new MyLibrary();
+            Book book1 = new Book("title1", "content1", "r author1", " category1");
+            Book book2 = new Book("title2", "content2", "a author2", " category2");
+            Book book3 = new Book("title3", "content3", "b author3", " category3");
+
+            myLibrary.AddBook(book1);
+            myLibrary.AddBook(book2);
+            myLibrary.AddBook(book3);
+            myLibrary.AddBook(book3);
+
+            Console.WriteLine(myLibrary.ToString());
+
+
+            Console.WriteLine(myLibrary.GetBookByAuthor("a author2").ToString());
+
+            List<Book> books = myLibrary.GetBooksSortedByAuthorName();
+            display_books(books);
+            Console.ReadLine();
+
+        }
+    }
+}
